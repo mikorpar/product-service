56c06b4585a4f3d29370ccd96c49bf449784cd69
miljenko <miljenko.korpar@gmail.com>
2025-07-30 01:09:54 +0200

    implement exchange rate service cache test

---
+++ src/test/java/com/mkorpar/productservice/services/ExchangeRateServiceCacheIntegrationTest.java
@@ -0,0 +1,67 @@
+package com.mkorpar.productservice.services;
+
+import com.mkorpar.productservice.clients.ExchangeRateApiClient;
+import com.mkorpar.productservice.clients.enums.ExchangeRateCurrency;
+import com.mkorpar.productservice.data.api.ExchangeRateApiResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class ExchangeRateServiceCacheIntegrationTest {
+
+    private static final String CACHE_NAME = "exchangeRates";
+    private static final LocalDate DATE = LocalDate.of(2025, 1, 1);
+
+    @MockitoBean
+    private ExchangeRateApiClient exchangeRateApiClient;
+
+    @Autowired
+    private ExchangeRateService exchangeRateService;
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @BeforeEach
+    void clearCache() {
+        Optional.ofNullable(cacheManager.getCache(CACHE_NAME))
+                .ifPresent(Cache::clear);
+    }
+
+    @Test
+    void shouldReturnCachedValue_WhenApiCallIsRepeatedWithTheSameDate() {
+        // Arrange
+        BigDecimal expectedRate = new BigDecimal("1.10");
+        ExchangeRateApiResponse response = new ExchangeRateApiResponse();
+        response.setMiddleRate(expectedRate);
+
+        when(exchangeRateApiClient.getExchangeRateAgainstEuro(ExchangeRateCurrency.USD, DATE))
+                .thenReturn(response);
+
+        // Act && Assert
+        verifyNoInteractions(exchangeRateApiClient);
+
+        exchangeRateService.getEurToUsdExchangeRate(DATE);
+        verify(exchangeRateApiClient, times(1))
+                .getExchangeRateAgainstEuro(ExchangeRateCurrency.USD, DATE);
+
+        exchangeRateService.getEurToUsdExchangeRate(DATE);
+        verify(exchangeRateApiClient, times(1))
+                .getExchangeRateAgainstEuro(ExchangeRateCurrency.USD, DATE);
+    }
+
+}
+
